228a7169a53d35ff90ec43bd748cb153a32c29b7
Celine Schlatter <celine.a.schlatter@bluewin.ch>
2021-05-04 10:53:09 +0200

    Task 3 completed

--- Exercise 2.R
+++ Exercise 2.R
@@ -11,7 +11,7 @@ library(lubridate)    # To handle dates and times
 
 ## Import the downloaded csv ###################################################
 
-wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",") # adjust path
+wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",")
 
 wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
 
@@ -19,27 +19,167 @@ wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, rem
 
 #Calculate the time difference between subsequent rows as described in the demo
 
-wildschwein_BE<- mutate(wildschwein_BE, timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units(secs))))
+wildschwein_BE<-wildschwein_BE%>%
+  group_by(TierID)%>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+View(wildschwein_BE)
 
 #Now inspect your data in more detail. Try to answer the following questions:
 
-#How many individuals were tracked?
+##How many individuals were tracked?
 table(wildschwein_BE$TierID)
-#three individuals were tracked 
 
 ggplot(data=wildschwein_BE)+
   geom_line(mapping=aes(x=wildschwein_BE$DatetimeUTC, y=wildschwein_BE$TierID))
+#three individuals were tracked 
+
+
+##For how long were the individual tracked? Are there gaps?
+Max_a<-  max(wildschwein_BE$DatetimeUTC)
+Max_a
+Min_a <- min(wildschwein_BE$DatetimeUTC)
+difftime(Max_a,Min_a,units = "days")
+#the individuals were tracked for 338.6 days
+
+
+##Were all individuals tracked concurrently or sequentially?
+ggplot(data=wildschwein_BE)+
+  geom_line(mapping=aes(x=wildschwein_BE$DatetimeUTC,y=wildschwein_BE$timelag,colour=wildschwein_BE$TierID))
+#They were tracked concurrently 
+
+
+
+##What is the temporal sampling interval between the locations?
+
+wildschwein_BE%>%
+  group_by(TierID)%>%
+  summarise(median(timelag,na.rm=TRUE))
 
 ggplot(data=wildschwein_BE)+
-  geom_histogram(mapping=aes(x=wildschwein_BE$timelag,y=count(wildschwein_BE$TierName)))
-?count
+  geom_bar(mapping=aes(x=timelag))+
+  scale_y_log10()+
+  xlim(0,15000)+
+  theme_classic()
 
+#the median for all individuals is around 900
+
+
+
+####Task 2 - Driving movement parameters I Speed################################
+
+#calculating eclidean distance (steplength)
+wildschwein_BE <- wildschwein_BE %>%
+  group_by(TierID) %>%
+  mutate(euclidean = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
 
 View(wildschwein_BE)
-#For how long were the individual tracked? Are there gaps?
-#Were all individuals tracked concurrently or sequentially?
-#What is the temporal sampling interval between the locations?
 
-wildschwein_BE
+#calculating speed
+wildschwein_BE <- wildschwein_BE %>%
+  mutate(speed=euclidean/timelag)
 
+View(wildschwein_BE)
+
+#speed unit = m/S
+
+
+####Task 3 - Cross-scale movement analysis######################################
+
+caro <- read_delim("caro60.csv",",")
+caro <- st_as_sf(caro, coords = c("E", "N"), crs = 2056, remove = FALSE)
+
+View(caro)
+
+caro_3 <- caro[seq(1,nrow(caro),by=3),]
+caro_6 <- caro[seq(1,nrow(caro),by=6),]
+caro_9 <- caro[seq(1,nrow(caro),by=9),]
+
+nrow(caro)
+nrow(caro_3)
+nrow(caro_6)
+nrow(caro_9)
+
+##Timelag
+caro<-caro%>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+caro_3<-caro_3%>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+caro_6<-caro_6%>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+caro_9<-caro_9%>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+View(caro_3)
+
+#calculating eclidean distance (steplength)
+caro <- caro %>%
+  mutate(euclidean = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+
+caro_3 <- caro_3 %>%
+  mutate(euclidean = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+
+caro_6<- caro_6 %>%
+  mutate(euclidean = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+
+caro_9 <- caro_9 %>%
+  mutate(euclidean = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
+
+#calculating speed
+caro <- caro %>%
+  mutate(speed=euclidean/timelag)
+
+caro_3 <- caro_3 %>%
+  mutate(speed=euclidean/timelag)
+
+caro_6 <- caro_6 %>%
+  mutate(speed=euclidean/timelag)
+
+caro_9 <- caro_9 %>%
+  mutate(speed=euclidean/timelag)
+
+View(caro_9)
+
+##Line plots
+
+ggplot(data=caro,mapping=aes(x=E, y=N))+
+  geom_path(data=caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro_3, mapping=aes(color="3 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro_3, mapping=aes(color="3 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 3 minutes-resampled data")+
+  theme(title=element_text(size=8))
+
+ggplot(data = caro,mapping=aes(x=E, y=N))+
+  geom_path(data= caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro_6, mapping=aes(color="6 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro_6, mapping=aes(color="6 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 6 minutes-resampled data")+
+  theme(title=element_text(size=8))
+
+ggplot(data = caro,mapping=aes(x=E, y=N))+
+  geom_path(data= caro,mapping=aes(color="1 minute"))+
+  geom_path(data = caro_9, mapping=aes(color="9 minutes"))+
+  geom_point(data=caro,mapping=aes(color="1 minute"))+
+  geom_point(data = caro_9, mapping=aes(color="9 minutes"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing original- with 9 minutes-resampled data")+
+  theme(title=element_text(size=8))
+
+ggplot(data = caro,mapping=aes(x=DatetimeUTC, y=speed))+
+  geom_line(data=caro, mapping=aes(colour="1 minute"))+
+  geom_line(data=caro_3, mapping=aes(colour="3 minute"))+
+  geom_line(data=caro_6, mapping=aes(colour="6 minute"))+
+  geom_line(data=caro_9, mapping=aes(colour="9 minute"))+
+  theme_light()+
+  labs(color="Trajectory", title = "Comparing derived speed at different sampling intervals")+
+  theme(title=element_text(size=8))+
+  xlab("Time")+
+  ylab("Speed (m/s)")
 
